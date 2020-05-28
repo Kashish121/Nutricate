9bdfc0dfef9a4eddcd450dea535aee5230757552
Kashish121 <56366103+Kashish121@users.noreply.github.com>
2020-05-28 22:05:53 +0530

    getNutrients working

--- apijquery.js
+++ apijquery.js
@@ -1,7 +1,9 @@
 // document.getElementById('getjson').addEventListener('click', getjson());
 function getjson1(){
+    var query = document.getElementById('findfor').value;
+    var url = "https://api.spoonacular.com/recipes/guessNutrition?title="+query+"&apiKey=35d3f922c81b4f0aa8297c313f1934cb"
 var settings = {
-    "url": "https://api.spoonacular.com/recipes/guessNutrition?title=milk&apiKey=35d3f922c81b4f0aa8297c313f1934cb",
+    "url": url,
     "method": "GET",
     "timeout": 0,
     "headers": {
@@ -12,6 +14,9 @@ var settings = {
   $.ajax(settings).done(function (response) {
     console.log(response);
     var obj=response;
-    document.getElementById('output').innerHTML=obj.carbs.value;
+    document.getElementById('1').innerHTML=obj.calories.value+" "+obj.calories.unit;
+    document.getElementById('2').innerHTML=obj.fat.value+" "+obj.fat.unit;
+    document.getElementById('3').innerHTML=obj.protein.value+" "+obj.protein.unit;
+    document.getElementById('4').innerHTML=obj.carbs.value+" "+obj.carbs.unit;
   });
 }
